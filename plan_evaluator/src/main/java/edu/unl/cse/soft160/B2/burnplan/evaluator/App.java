50728e61cd7934a1da3e6f76dfdc43428e5f1352
bjohnson261 <bjohnson261@huskers.unl.edu>
2023-11-13 14:58:12 -0600

    Inital Commit

---
+++ plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/App.java
@@ -0,0 +1,13 @@
+package edu.unl.cse.soft160.B2.burnplan.evaluator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
