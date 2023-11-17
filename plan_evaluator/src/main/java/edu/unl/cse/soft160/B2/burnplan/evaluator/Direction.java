dc85d41e4cd70380055f1c2328bbdf5e73ef6756
rsolberg3 <rsolberg3@huskers.unl.edu>
2023-11-17 11:56:01 -0600

    Saved all the files before pushing this time.

--- plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/Direction.java
+++ plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/Direction.java
@@ -1,5 +1,5 @@
 package edu.unl.cse.soft160.B2.burnplan.evaluator;
 
 public enum Direction {
-
+	NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST
 }
--- plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/Season.java
+++ plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/Season.java
@@ -1,5 +1,5 @@
 package edu.unl.cse.soft160.B2.burnplan.evaluator;
 
 public enum Season {
-
+	SUMMER, FALL, WINTER, SPRING
 }
