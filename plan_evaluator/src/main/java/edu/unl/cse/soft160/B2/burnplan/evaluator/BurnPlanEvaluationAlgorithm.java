2d63b34acea8ced1f8b7ab02188a6b4309142c2a
salsayid2 <salsayid2@huskers.unl.edu>
2023-11-29 11:45:11 -0600

    merged weather connector

--- plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/BurnPlanEvaluationAlgorithm.java
+++ plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/BurnPlanEvaluationAlgorithm.java
@@ -1,6 +1,6 @@
 package edu.unl.cse.soft160.B2.burnplan.evaluator;
 
-public class BurnPlanEvaluationAlgorithm {
+public class BurnPlanEvaluationAlgorithm { 
 	public static BurnDetermination evaluate(BurnPlan burnPlan) {
 		return BurnDetermination.INDETERMINATE; // stub
 	}
