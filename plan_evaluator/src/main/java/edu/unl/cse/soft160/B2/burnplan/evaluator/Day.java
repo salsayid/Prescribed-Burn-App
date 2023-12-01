e57034d16192fbde37d435561b338e4c4a3033a7
rsolberg3 <rsolberg3@huskers.unl.edu>
2023-12-01 11:41:58 -0600

    Removed time of day because date covers time.

--- plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/Day.java
+++ plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/Day.java
@@ -1,19 +1,17 @@
 package edu.unl.cse.soft160.B2.burnplan.evaluator;
 
 import java.util.Date;
-import java.time.LocalTime;
+
 
 public class Day {
 	private boolean isOutdoorBuringBanned;
-	private LocalTime timeOfDay; //hour:minute
 	private Season season;
 	private Weather weather;
 	private Date date;
 		
-	public Day(Date date, Weather weather, boolean isOutdoorBurningBanned, LocalTime timeOfDay, Season season) {
+	public Day(Date date, Weather weather, boolean isOutdoorBurningBanned, Season season) {
 		this.weather = weather;
 		this.isOutdoorBuringBanned = isOutdoorBurningBanned;
-		this.timeOfDay = timeOfDay;
 		this.season = season;
 		this.date = date;
 	}
@@ -24,14 +22,6 @@ public class Day {
 
 	public void setOutdoorBuringBanned(boolean isOutdoorBuringBanned) {
 		this.isOutdoorBuringBanned = isOutdoorBuringBanned;
-	}
-
-	public LocalTime getTimeOfDay() {
-		return timeOfDay;
-	}
-
-	public void setTimeOfDay(LocalTime timeOfDay) {
-		this.timeOfDay = timeOfDay;
 	}
 
 	public Season getSeason() {
