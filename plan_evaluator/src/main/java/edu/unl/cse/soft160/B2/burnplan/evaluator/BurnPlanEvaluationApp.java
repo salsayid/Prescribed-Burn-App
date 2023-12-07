cccbd180169b305a24b947537f8998980afe3cb4
Connor Fausett <cfausett2@huskers.unl.edu>
2023-12-06 20:21:17 -0600

    Added "Successfully exited." string to the Supplies prompt section.

--- plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/BurnPlanEvaluationApp.java
+++ plan_evaluator/src/main/java/edu/unl/cse/soft160/B2/burnplan/evaluator/BurnPlanEvaluationApp.java
@@ -454,6 +454,7 @@ public class BurnPlanEvaluationApp {
 				}
 			}
 			if (input.equals("exit")) {
+				System.out.println("Successfully exited.");
 				System.exit(1);
 			}
 		} while (!haveAllInputs);
